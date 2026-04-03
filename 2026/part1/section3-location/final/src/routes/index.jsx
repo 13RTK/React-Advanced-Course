5f102caea44b947edfa967c3063ecb77a60ee95c
alex <907785012@qq.com>
2026-04-03 21:45:03 +0800

    fix

--- 2026/part1/section3-location/final/src/routes/index.jsx
+++ 2026/part1/section3-location/final/src/routes/index.jsx
@@ -1,15 +1,22 @@
 import { createBrowserRouter } from 'react-router';
 import Home from '../components/Home';
 import About from '../components/About';
+import App from '../App';
 
 const router = createBrowserRouter([
   {
     path: '/',
-    Component: Home,
-  },
-  {
-    path: '/about',
-    Component: About,
+    Component: App,
+    children: [
+      {
+        index: true,
+        Component: Home,
+      },
+      {
+        path: 'about',
+        Component: About,
+      },
+    ],
   },
 ]);
 
