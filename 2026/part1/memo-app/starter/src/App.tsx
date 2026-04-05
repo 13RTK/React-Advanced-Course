aeab2a6f07289aa8bb0eca374441e148c4dba31f
alex <907785012@qq.com>
2026-04-05 22:59:05 +0800

    complete memo-app 01

---
+++ 2026/part1/memo-app/starter/src/App.tsx
@@ -0,0 +1,12 @@
+import ButtonUsage from './components/ButtonUsage';
+import Navbar from './components/Navbar';
+
+function App() {
+  return (
+    <>
+      <Navbar />
+    </>
+  );
+}
+
+export default App;
