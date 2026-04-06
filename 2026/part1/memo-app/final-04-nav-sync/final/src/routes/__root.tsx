49da30b8e76125e41dcb2fa174efcff6edac6f75
alex <907785012@qq.com>
2026-04-06 22:55:33 +0800

    complete final-04

---
+++ 2026/part1/memo-app/final-04-nav-sync/final/src/routes/__root.tsx
@@ -0,0 +1,22 @@
+import { createRootRoute, Outlet } from '@tanstack/react-router';
+import { TanStackRouterDevtools } from '@tanstack/react-router-devtools';
+import BottomNav from '../components/BottomNav';
+import Navbar from '../components/Navbar';
+
+const RootLayout = () => (
+  <>
+    <Navbar />
+    <main
+      style={{
+        marginBottom: '56px',
+        marginTop: '56px',
+      }}
+    >
+      <Outlet />
+    </main>
+    <BottomNav />
+    <TanStackRouterDevtools />
+  </>
+);
+
+export const Route = createRootRoute({ component: RootLayout });
