eaf9c37ae2add61d7fec02a72a782d1bc9ede76b
alex <907785012@qq.com>
2026-04-08 22:04:05 +0800

    add starter of final-08

---
+++ 2026/part1/memo-app/final-08-delete/final/src/components/MemoList.tsx
@@ -0,0 +1,103 @@
+import List from '@mui/material/List';
+import MemoListItem from './MemoListItem';
+import { useMemoList } from '../hooks/memoList';
+import { Link } from '@tanstack/react-router';
+import { useEffect, useState } from 'react';
+import type { MemoItem } from '../types/MemoItem';
+
+export default function MemoList({ search = '' }) {
+  // const mockMemoList: MemoItem[] = [
+  //   {
+  //     id: 1,
+  //     title: 'Brunch this weekend?',
+  //     content:
+  //       "I'll be in your neighborhood doing errands this weekend. Do you want to hang out?",
+  //   },
+  //   {
+  //     id: 2,
+  //     title: 'Brunch this weekend?',
+  //     content:
+  //       "I'll be in your neighborhood doing errands this weekend. Do you want to hang out?",
+  //   },
+  //   {
+  //     id: 3,
+  //     title: 'Brunch this weekend?',
+  //     content:
+  //       "I'll be in your neighborhood doing errands this weekend. Do you want to hang out?",
+  //   },
+  //   {
+  //     id: 4,
+  //     title: 'Brunch this weekend?',
+  //     content:
+  //       "I'll be in your neighborhood doing errands this weekend. Do you want to hang out?",
+  //   },
+  //   {
+  //     id: 5,
+  //     title: 'Brunch this weekend?',
+  //     content:
+  //       "I'll be in your neighborhood doing errands this weekend. Do you want to hang out?",
+  //   },
+  //   {
+  //     id: 11,
+  //     title: 'Brunch this weekend?',
+  //     content:
+  //       "I'll be in your neighborhood doing errands this weekend. Do you want to hang out?",
+  //   },
+  //   {
+  //     id: 21,
+  //     title: 'Brunch this weekend?',
+  //     content:
+  //       "I'll be in your neighborhood doing errands this weekend. Do you want to hang out?",
+  //   },
+  //   {
+  //     id: 31,
+  //     title: 'Brunch this weekend?',
+  //     content:
+  //       "I'll be in your neighborhood doing errands this weekend. Do you want to hang out?",
+  //   },
+  //   {
+  //     id: 41,
+  //     title: 'Brunch this weekend?',
+  //     content:
+  //       "I'll be in your neighborhood doing errands this weekend. Do you want to hang out?",
+  //   },
+  //   {
+  //     id: 51,
+  //     title: 'Brunch this weekend?',
+  //     content:
+  //       "I'll be in your neighborhood doing errands this weekend. Do you want to hang out?",
+  //   },
+  // ];
+
+  const { memoList } = useMemoList();
+  const [processedMemoList, setProcessedMemoList] = useState<MemoItem[]>([]);
+
+  useEffect(() => {
+    if (!search) {
+      setProcessedMemoList(memoList || []);
+    }
+
+    const filteredMemoList = memoList?.filter((memo) => {
+      return memo.title.toLowerCase().includes(search.toLowerCase());
+    });
+    setProcessedMemoList(filteredMemoList || []);
+  }, [search]);
+
+  return (
+    <>
+      {processedMemoList.length === 0 && (
+        <h1 style={{ textAlign: 'center' }}>
+          There's no memo, try to
+          <Link to="/add"> Add </Link>
+          one
+        </h1>
+      )}
+
+      <List sx={{ width: '100%', bgcolor: 'background.paper' }}>
+        {processedMemoList.map((memoItem) => (
+          <MemoListItem key={memoItem.id} memoItem={memoItem} />
+        ))}
+      </List>
+    </>
+  );
+}
