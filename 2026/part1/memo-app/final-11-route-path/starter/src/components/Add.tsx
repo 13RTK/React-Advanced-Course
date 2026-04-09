b183df7f0d13d98a8cca919228893f6c9caae7c6
alex <907785012@qq.com>
2026-04-10 00:25:02 +0800

    add starter of final-11

---
+++ 2026/part1/memo-app/final-11-route-path/starter/src/components/Add.tsx
@@ -0,0 +1,61 @@
+import { Button, TextField } from '@mui/material';
+import { useState } from 'react';
+import { useMemoList } from '../hooks/memoList';
+import { toast } from 'sonner';
+
+function Add() {
+  const [title, setTitle] = useState('');
+  const [content, setContent] = useState('');
+
+  const { memoList, setMemoList } = useMemoList();
+
+  function handleSubmit(event: React.SubmitEvent<HTMLFormElement>) {
+    event.preventDefault();
+
+    const newMemoList = [
+      ...(memoList || []),
+      {
+        id: Date.now(),
+        title,
+        content,
+      },
+    ];
+    setMemoList(newMemoList);
+    toast.success('memo added');
+
+    setTitle('');
+    setContent('');
+  }
+
+  return (
+    <form style={{ textAlign: 'center' }} onSubmit={handleSubmit}>
+      <TextField
+        sx={{ marginBottom: '10px', marginTop: '10px' }}
+        id="outlined-basic"
+        label="title"
+        variant="outlined"
+        value={title}
+        onChange={(event) => setTitle(event.target.value)}
+      />
+
+      <br />
+
+      <TextField
+        id="outlined-multiline-static"
+        sx={{ marginBottom: '10px', marginTop: '10px' }}
+        label="content"
+        multiline
+        rows={4}
+        value={content}
+        onChange={(event) => setContent(event.target.value)}
+      />
+
+      <br />
+      <Button type="submit" variant="contained" size="large">
+        Add
+      </Button>
+    </form>
+  );
+}
+
+export default Add;
