69540f1e5f95f6469d918ddd08bf79fa415af814
alex <907785012@qq.com>
2026-04-13 22:38:59 +0800

    complete final-02

--- 2026/part2/shopping-list/final02-theme/final/src/components/Navbar.tsx
+++ 2026/part2/shopping-list/final02-theme/final/src/components/Navbar.tsx
@@ -1,8 +1,26 @@
 import { Menubar } from 'primereact/menubar';
 import { InputText } from 'primereact/inputtext';
 import { Badge } from 'primereact/badge';
+import { Button } from 'primereact/button';
+import { useContext, useState } from 'react';
+import { PrimeReactContext } from 'primereact/api';
 
 export default function Navbar() {
+  const { changeTheme } = useContext(PrimeReactContext);
+  const [isDark, setIsDark] = useState(false);
+
+  const LIGHT_THEME = 'lara-light-cyan';
+  const DARK_THEME = 'lara-dark-cyan';
+
+  function toggleTheme() {
+    const currentTheme = isDark ? DARK_THEME : LIGHT_THEME;
+    const newTheme = isDark ? LIGHT_THEME : DARK_THEME;
+
+    changeTheme?.(currentTheme, newTheme, 'theme-link', () => {
+      setIsDark(!isDark);
+    });
+  }
+
   const itemRenderer = (item: any) => (
     <a className="flex align-items-center p-menuitem-link">
       <span className={item.icon} />
@@ -32,12 +50,20 @@ export default function Navbar() {
       className="mr-2"
     ></img>
   );
+
   const end = (
     <div className="flex align-items-center gap-2">
       <InputText
         placeholder="Search"
         type="text"
         className="w-8rem sm:w-auto"
+      />
+      <Button
+        onClick={toggleTheme}
+        icon={`pi pi-${isDark ? 'moon' : 'sun'}`}
+        aria-label="Filter"
+        text
+        severity="secondary"
       />
     </div>
   );
