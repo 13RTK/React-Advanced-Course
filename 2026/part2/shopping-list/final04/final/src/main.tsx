e3d64e4c6709c69d00d9668fba009363bd1be0eb
alex <907785012@qq.com>
2026-04-15 14:39:20 +0800

    add starter of final04

---
+++ 2026/part2/shopping-list/final04/final/src/main.tsx
@@ -0,0 +1,18 @@
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.tsx';
+import { PrimeReactProvider } from 'primereact/api';
+
+import 'primeicons/primeicons.css';
+import 'primeflex/primeflex.css';
+
+import './index.css';
+import './flags.css';
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <PrimeReactProvider>
+      <App />
+    </PrimeReactProvider>
+  </StrictMode>,
+);
---
+++ 2026/part2/shopping-list/final04/starter/src/App.tsx
@@ -0,0 +1,13 @@
+import Navbar from './components/Navbar';
+import ShopList from './components/ShopList';
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <ShopList />
+    </>
+  );
+}
+
+export default App;
