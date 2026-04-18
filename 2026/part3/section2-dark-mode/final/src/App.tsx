643af1f41b7f3e8bd0f809ab64425c1ae17674c8
alex <907785012@qq.com>
2026-04-18 14:54:19 +0800

    add starter of section3-2

---
+++ 2026/part3/section2-dark-mode/final/src/App.tsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <div className="text-black text-center">
+      <nav className="text-3xl bg-slate-200 font-serif font-bold">
+        Hello world!
+      </nav>
+
+      <main>Main Content</main>
+    </div>
+  );
+}
+
+export default App;
