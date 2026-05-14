146802e940ff951fe2cc1d44d434b9d796e76ebe
alex <907785012@qq.com>
2026-05-14 20:01:27 +0800

    complete final45

--- 2026/Aedium/final45-article-brief/src/components/ArticleListItem.tsx
+++ 2026/Aedium/final45-article-brief/src/components/ArticleListItem.tsx
@@ -2,6 +2,57 @@ import { HeartIcon } from '@phosphor-icons/react';
 import type { Article } from '../types/Article';
 
 function ArticleListItem({ article }: { article: Article }) {
+  function getArticleBrief(content: string, maxLength = 100) {
+    const blocks = JSON.parse(content);
+
+    let brief = '';
+
+    brief = blocks
+      .map((block: any) => {
+        let ownText = '';
+        let childrenText = '';
+
+        if (Array.isArray(block.content)) {
+          ownText = block.content
+            .map((item: any) => {
+              if (item.type === 'text') {
+                return item.text;
+              }
+
+              if (item.type === 'link') {
+                if (Array.isArray(item.content)) {
+                  item.content.map((child: any) => child.text ?? '').join(' ');
+                }
+              }
+
+              return '';
+            })
+            .join(' ');
+        }
+
+        if (Array.isArray(block.children)) {
+          childrenText = block.children
+            .map((childBlock: any) => {
+              getArticleBrief(JSON.stringify([childBlock]), maxLength);
+            })
+            .join(' ');
+        }
+
+        return `${ownText} ${childrenText}`.trim();
+      })
+      .join(' ')
+      .replace(/\s+/g, ' ')
+      .trim();
+
+    if (!brief) {
+      brief = 'No brief';
+    }
+
+    return brief.length > maxLength
+      ? brief.slice(0, maxLength).trim() + '...'
+      : brief;
+  }
+
   return (
     <li className="list-row h-40">
       <div>
@@ -18,7 +69,7 @@ function ArticleListItem({ article }: { article: Article }) {
 
         {/* Content brief */}
         <div className="sm:text-2xl text-sm font-semibold opacity-60">
-          Remaining Reason
+          {getArticleBrief(article.content)}
         </div>
       </div>
 
