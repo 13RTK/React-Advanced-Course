644cc2d760d5f6e2c5ea21f3e22a8d435826926e
alex <907785012@qq.com>
2026-05-14 23:13:40 +0800

    complete final48

--- 2026/Aedium/final48-article-menu/src/components/Article.tsx
+++ 2026/Aedium/final48-article-menu/src/components/Article.tsx
@@ -4,6 +4,13 @@ import { useQuery } from '@tanstack/react-query';
 import { getArticleById } from '../services/apiArticle';
 import Loading from './Loading';
 import AppEditor from './AppEditor';
+import {
+  BookmarkIcon,
+  PencilCircleIcon,
+  ThumbsUpIcon,
+  TrashIcon,
+} from '@phosphor-icons/react';
+import { SignedIn } from '@neondatabase/neon-js/auth/react';
 
 function Article() {
   const { articleId } = ArticleRoute.useParams();
@@ -28,6 +35,36 @@ function Article() {
   return (
     <>
       <h1 className="text-center text-7xl font-serif mb-8">{article?.title}</h1>
+
+      <SignedIn>
+        <div className="divider"></div>
+
+        <div className="flex justify-center">
+          <ul className="menu menu-horizontal rounded-box">
+            <li>
+              <a className="tooltip mx-1" data-tip="Like">
+                <ThumbsUpIcon size={24} weight="thin" />
+              </a>
+            </li>
+            <li>
+              <a className="tooltip mx-1" data-tip="Bookmark">
+                <BookmarkIcon size={24} weight="thin" />
+              </a>
+            </li>
+            <li>
+              <a className="tooltip bg-blue-600 mx-1" data-tip="Edit">
+                <PencilCircleIcon size={24} weight="thin" />
+              </a>
+            </li>
+            <li>
+              <a className="tooltip bg-red-600 mx-1" data-tip="Delete">
+                <TrashIcon size={24} weight="thin" />
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div className="divider"></div>
+      </SignedIn>
 
       <AppEditor editor={editor} editable={false} />
     </>
