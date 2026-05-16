31470999322cb3db79846c442267f82b1876bedc
alex <907785012@qq.com>
2026-05-16 20:03:17 +0800

    complete final51

---
+++ 2026/Aedium/final51-article-delete/src/routes/_app/_protected/editor.tsx
@@ -0,0 +1,6 @@
+import { createFileRoute } from '@tanstack/react-router';
+import ArticleEditor from '@/components/ArticleEditor';
+
+export const Route = createFileRoute('/_app/_protected/editor')({
+  component: ArticleEditor,
+});
