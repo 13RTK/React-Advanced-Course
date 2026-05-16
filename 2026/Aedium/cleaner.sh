67c042f789f3e0b8b6bb26b5c205fe4e41694d4d
alex <907785012@qq.com>
2026-05-16 19:45:51 +0800

    add cleaner in project to exclude older sections

---
+++ 2026/Aedium/cleaner.sh
@@ -0,0 +1,8 @@
+# Find all the node modules paths and delete all
+find ./ -name "node_modules" -type d -exec rm -rf {} +
+
+# Find all pnpm-lock.yaml files and delete them
+# find ./ -name "pnpm-lock.yaml" -type f -exec rm -rf {} +
+
+# Find all dist folders and delete them
+find ./ -name "dist" -type d -exec rm -rf {} +
